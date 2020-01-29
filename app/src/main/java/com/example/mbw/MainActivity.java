047a463ae49c467cbc597c6abbab7ee4b5baf0e1
yjsohn <panda_8@naver.com>
2020-01-29 13:41:37 +0900

    google map places까지 했음

--- app/src/main/java/com/example/mbw/MainActivity.java
+++ app/src/main/java/com/example/mbw/MainActivity.java
@@ -4,20 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapPOIItem;
+import com.skt.Tmap.TMapTapi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 //google api: AIzaSyB9Mr6iX5Dm-Xck6i_LKLhbVvZVcQ8dFyY
 public class MainActivity extends AppCompatActivity {
 
-    Intent intent = null;
+    Intent intent = null, searchIntent = null;
     EditText departure, destination;
     TextView toHome, toOffice;
     ImageView profile, swap, home, office, bookmark;
+    int AUTOCOMPLETE_REQUEST_CODE = 1;
+    // Set the fields to specify which types of place data to
+    // return after the user has made a selection.
+    List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ADDRESS);
+    String searchResult = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +43,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         departure = findViewById(R.id.departureText);
         destination = findViewById(R.id.destinationText);
-        Places.initialize(getApplicationContext(), "AIzaSyB9Mr6iX5Dm-Xck6i_LKLhbVvZVcQ8dFyY");
+        //Places.initialize(getApplicationContext(), "AIzaSyB9Mr6iX5Dm-Xck6i_LKLhbVvZVcQ8dFyY");
+        TMapTapi tmaptapi = new TMapTapi(this);
+        tmaptapi.setSKTMapAuthentication ("l7xxbcd1d4f9f0984e8b99466490a2b372b7");
+
         //PlacesClient placesClient = Places.createClient(this);
 
         /*toHome = findViewById(R.id.toHome);
@@ -68,9 +89,50 @@ public class MainActivity extends AppCompatActivity {
                 intent = new Intent(MainActivity.this, BookmarkActivity.class);
                 startActivity(intent);
                 break;
-
+            case R.id.departureText:
+                TMapData tmapdata = new TMapData();
+                //	ArrayList POIItem = tmapdata.findAllPOI("SKT타워");
+                tmapdata.findAllPOI(departure.getText().toString(), new TMapData.FindAllPOIListenerCallback() {
+                    @Override
+                    public void onFindAllPOI(ArrayList poiItem) {
+                        for(int i = 0; i < poiItem.size(); i++) {
+                            TMapPOIItem item = (TMapPOIItem) poiItem.get(i);
+                            Log.d("POI Name: ", item.getPOIName().toString() + ", " +
+                                    "Address: " + item.getPOIAddress().replace("null", "")  + ", " +
+                                    "Point: " + item.getPOIPoint().toString());
+                        }
+                    }
+                });
+                break;
+            case R.id.destinationText:
+                // Start the autocomplete intent.
+                /*searchResult = destination.getText().toString();
+                searchIntent = new Autocomplete.IntentBuilder(
+                        AutocompleteActivityMode.OVERLAY, fields)
+                        .build(this);
+                startActivityForResult(searchIntent, AUTOCOMPLETE_REQUEST_CODE);
+                destination.setText(searchResult);*/
+                break;
         }
     }
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                searchResult = place.getName();
+                //Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // TODO: Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+                //Log.i(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }*/
 
     public void onExample(View v){
         intent = new Intent(MainActivity.this, TestActivity.class);
