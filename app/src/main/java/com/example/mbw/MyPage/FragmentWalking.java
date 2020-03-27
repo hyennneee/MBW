4de106152a519020ed97ea75c01cd0737bc69668
yjsohn <panda_8@naver.com>
2020-03-27 16:40:54 +0900

    도보 속력 조정 기능 추가

--- app/src/main/java/com/example/mbw/MyPage/FragmentWalking.java
+++ app/src/main/java/com/example/mbw/MyPage/FragmentWalking.java
@@ -1,10 +1,18 @@
 package com.example.mbw.MyPage;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,6 +56,41 @@ public class FragmentWalking extends Fragment {
         }
         walkingSpeed.setText("" + speed);
 
+        walkingSpeed.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    InputMethodManager inputMethodManager =(InputMethodManager)getContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        });
+        walkingSpeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((EditText)view).setInputType(EditorInfo.TYPE_CLASS_NUMBER); // setCursorVisible(true); 도 가능하다.
+            }
+
+        });
+
+        speedBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (walkingSpeed.isFocused()) {
+                        Rect outRect = new Rect();
+                        walkingSpeed.getGlobalVisibleRect(outRect);
+                        if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                            walkingSpeed.clearFocus();
+                            InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                        }
+                    }
+                }
+                return false;
+            }
+        });
+
         speedBtn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,6 +103,7 @@ public class FragmentWalking extends Fragment {
                     speedDBHelper.insertSpeed(0, newSpeed);
                     Toast.makeText(getContext(), "등록되었습니다", Toast.LENGTH_SHORT).show();
                     walkingSpeed.setText("" + newSpeed);
+                    walkingSpeed.setInputType(EditorInfo.TYPE_NULL);
                     }
                     catch (Exception e) {
                         e.printStackTrace();
