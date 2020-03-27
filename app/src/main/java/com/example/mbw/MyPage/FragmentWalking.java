d299553c2db12a4aa95eebc869594eba151229a2
yjsohn <panda_8@naver.com>
2020-03-27 15:55:33 +0900

    도보 속력 조정 기능 추가

---
+++ app/src/main/java/com/example/mbw/MyPage/FragmentWalking.java
@@ -0,0 +1,72 @@
+package com.example.mbw.MyPage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.mbw.DB.SpeedDBHelper;
+import com.example.mbw.R;
+
+public class FragmentWalking extends Fragment {
+
+    TextView walkingSpeed;
+    Button speedBtn;
+    SpeedDBHelper speedDBHelper;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_walking, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        speedDBHelper = new SpeedDBHelper(getContext());
+        walkingSpeed = view.findViewById(R.id.walkingSpeedText);
+        speedBtn = view.findViewById(R.id.speedButton);
+
+        //현재 저장된 speed 값 가져오기
+        int cnt = speedDBHelper.count();
+        String speed;
+        if(cnt == 0){
+            speed = "2.1";
+            speedDBHelper.insertSpeed(0, speed);
+        }
+        else{
+            speed = speedDBHelper.getSpeed();
+        }
+        walkingSpeed.setText("" + speed);
+
+        speedBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newSpeed = walkingSpeed.getText().toString();
+                if(newSpeed.isEmpty()){
+                    Toast.makeText(getContext(), "사용자 도보 속력을 입력해주세요", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    try{
+                    speedDBHelper.insertSpeed(0, newSpeed);
+                    Toast.makeText(getContext(), "등록되었습니다", Toast.LENGTH_SHORT).show();
+                    walkingSpeed.setText("" + newSpeed);
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+}
